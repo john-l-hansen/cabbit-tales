e8e0e6d25fe2ac39aac5deb01aba056b4bc12de6
John Hansen <john.l.hansen@gmail.com>
2021-07-01 23:35:31 -0700

    add social icons and hero assets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import facebook from './img/facebook.svg';
+import instagram from './img/instagram.svg';
+import twitter from './img/twitter.svg';
+import youtube from './img/youtube.svg';
+import logo from './img/logo.png';
+
+import Button from './Components/Button.js';
 
 function App() {
   return (
     <div className="App">
+
+
+
+
+
+      {/* Working on top nav */}
+      <div className="Top-nav">
+        <a
+        href="https://google.com">
+          <img src={facebook} />
+        </a>
+        <a
+        href="https://google.com">
+          <img src={instagram} />
+        </a>
+        <a
+        href="https://google.com">
+          <img src={twitter} />
+        </a>
+        <a
+        href="https://google.com">
+          <img src={youtube} />
+        </a>
+      </div>
+      {/* End top nav */}
+
+
+
+
+
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cabbit Tales
+        Welcome to the wonderful world of Cabbit Tales! An interactive story app!
         </p>
         <a
           className="App-link"
@@ -15,7 +55,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Download on the App Store free today!
+          <Button>Download on the App Store free today!</Button>
         </a>
       </header>
     </div>
---
+++ src/Components/Button.js
@@ -0,0 +1,21 @@
+function App() {
+    return (
+      <div className="Button-primary">
+        <button style={{
+          backgroundColor: "#FBAD10",
+          borderRadius: "4px",
+          borderWidth: "0",
+          color: "purple",
+          fontSize: "34px",
+          padding: "1rem 3rem",
+        
+        }}
+        
+        
+        
+        >test</button>
+      </div>
+    );
+  }
+  
+  export default App;
